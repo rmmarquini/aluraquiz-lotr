a1854bb74b7b4d89a1b4f87138bba9a4a1809ced
Rafael Marquini <rafa.mardegan@gmail.com>
2021-01-26 21:16:26 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,56 @@
+import React from 'react'
+import Head from 'next/head'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
+import db from '../db.json'
+
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
   }
-`
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+ 
+        <meta name="description" content={db.description} />
+        <title>{db.title}</title>
+
+        {/* Open Graph */}
+        <meta property="og:url" content={db.url} key="ogurl" />
+        <meta property="og:image" content={db.bg} key="ogimage" />
+        <meta property="og:site_name" content={db.title} key="ogsitename" />
+        <meta property="og:title" content={db.title} key="ogtitle" />
+        <meta property="og:description" content={db.description} key="ogdesc" />
+
+      </Head>
+
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import QuizLogo from '../src/components/QuizLogo'
+import Footer from '../src/components/Footer'
+import GithubCorner from '../src/components/GithubCorner'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>LOTR Master Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h3>That You Shall Not Pass!</h3>
+            <p>
+              Despite being released well over a decade ago, The Lord of the Rings 
+              movies remains just as prevalent in our culture. It looks like 
+              this Tolkien fantasy isn’t going anywhere anytime soon, which is good 
+              for those us who like to venture back into Middle-earth at least 
+              a few times every year.
+            </p>
+            <p>
+              So for this quiz, we’ll be focusing on some more of the obscure trivia 
+              and facts from the original trilogy.
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Related Quizzes</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/rmmarquini" />
+    </QuizBackground>
+  )
 }
